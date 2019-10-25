6597a3d9c1240fbef48fe7053a6da966ee479f13
Grisha Petrosyan <Grisha_Petrosyan@epam.com>
2019-10-25 07:03:13 +0400

    Updates

---
+++ precache-manifest.73a3e8e20472bf39b4a29fd97b71a009.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52518173881a777eac823c3d12370a3d",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "1fc2bbd69a14786c4b95",
+    "url": "/react-weather-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "569a5777e514260dcc04",
+    "url": "/react-weather-app/static/js/2.05735afa.chunk.js"
+  },
+  {
+    "revision": "1fc2bbd69a14786c4b95",
+    "url": "/react-weather-app/static/js/main.2e7e8d8e.chunk.js"
+  },
+  {
+    "revision": "617bc780f24238f442b7",
+    "url": "/react-weather-app/static/js/runtime-main.14bf7698.js"
+  }
+]);
